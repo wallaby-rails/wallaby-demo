5b71384e0dd9b00de58e4a27104132b106003364
Tianwen (Tian) Chen <tian-im@users.noreply.github.com>
2025-04-27 17:42:33 +1000

    Try to fix migration (#8)

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_15_121838) do
+ActiveRecord::Schema[8.0].define(version: 2022_05_15_121838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "hstore"
   enable_extension "ltree"
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
