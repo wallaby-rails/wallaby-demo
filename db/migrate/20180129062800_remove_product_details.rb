9505cc077a97fea024d6ca895002bbc1515639bf
Tianwen (Tian) Chen <tian-im@users.noreply.github.com>
2025-04-27 18:13:05 +1000

    Fix migration again (#9)

--- db/migrate/20180129062800_remove_product_details.rb
+++ db/migrate/20180129062800_remove_product_details.rb
@@ -1,9 +1,11 @@
 class RemoveProductDetails < ActiveRecord::Migration[4.2][5.1]
+  disable_ddl_transaction!
+
   def change
     drop_table :product_details, if_exists: true
     drop_table :people, if_exists: true
 
-    add_column :users, :type, :string
-    add_column :orders, :order_number, :string
+    add_column :users, :type, :string, if_not_exists: true
+    add_column :orders, :order_number, :string, if_not_exists: true
   end
 end
